816b98bcf1a30017c26ed66afacb3022d40282f6
annie.hedgpeth <annie.hedgpeth@relativity.com>
2019-01-16 20:58:53 -0700

    through #8 basic

--- main/main.go
+++ main/main.go
@@ -2,11 +2,24 @@ package main
 
 import "fmt"
 
+var name = "Annie"
+
 func main() {
 	sayHi()
+	isOwner()
+	simpsonsMember := []string{"Homer", "Marge", "Bart", "Lisa", "Maggie"}
+	for name := 0; name < len(simpsonsMember); name++ {
+		person := simpsonsMember[name]
+		fmt.Printf("%d: %v\n", name, person)
+	}
 }
 
 func sayHi() {
-	name := "World"
-	fmt.Printf("Hello, %v!", name)
+	fmt.Printf("Hello, %v!\n", name)
+}
+
+func isOwner() {
+	if name == "Annie" {
+		fmt.Println("Your name is like the owner of this repo!")
+	}
 }
