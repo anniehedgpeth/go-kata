1a0a4d23bb2bf908400d75d1cddd1e0f24acb49c
levitiger <levi.hedgpeth@gmail.com>
2019-01-17 18:59:58 -0700

    'commit'

--- main/main.go
+++ main/main.go
@@ -1,44 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-var name = "Annie"
+var name = "Levi"
+
+func main(){
+	sayHi()
+}
 
 func sayHi() {
 	fmt.Printf("Hello, %v!\n", name)
-}
-
-func isOwner() {
-	if name == "Annie" {
-		fmt.Println("Your name is like the owner repo!")
+	if name == "Levi" {
+		fmt.Println("Your name is like the owner of this repo!")
 	}
-}
 
-func sayFavoriteColorsOfSimpsons() {
-	familymembers := map[string]string{"Homer": "blue", "Marge": "yellow", "Bart": "black", "Lisa": "red", "Maggie": "purple"}
-	for name, color := range familymembers {
-		fmt.Printf("%v: %v\n", name, color)
-	}
-}
-
-func sayBirthdayOfSimpsons() {
-	familymembers := map[string]int{"Homer": 1, "Marge": 2, "Bart": 3, "Lisa": 4, "Maggie": 5}
-	for name, date := range familymembers {
-		fmt.Printf("%v: %d\n", name, date)
-	}
-}
-
-func sayAllowancesOfSimpsons() {
-	familymembers := map[string]float64{"Homer": 10.5, "Marge": 20.6, "Bart": 3.4, "Lisa": 4.5, "Maggie": 2.5}
-	for name, allowance := range familymembers {
-		fmt.Printf("%v: $%2.2f\n", name, allowance)
-	}
-}
-
-func main() {
-	sayHi()
-	sayFavoriteColorsOfSimpsons()
-	sayBirthdayOfSimpsons()
-	sayAllowancesOfSimpsons()
-	isOwner()
 }
